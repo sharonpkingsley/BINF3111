e0ad393ad1fb3891c6aa4eac2214244b25a33b8d
GraceC1029 <LouiseC@GraceCdeMacBook-Air.local>
2017-09-11 11:00:59 +1000

    table view on result page, search with one evidence

--- combined/app.py
+++ combined/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, redirect, render_template, json, request, jsonify, url_for
 from flaskext.mysql import MySQL
 from forms import SearchForm
-from werkzeug import generate_password_hash, check_password_hash, secure_filename
+from werkzeug import secure_filename #generate_password_hash, check_password_hash, 
 import os
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
-#from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-#import tempfile
   
 #mysql setup
 mysql = MySQL()
@@ -17,7 +15,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'binf3111'
 app.config['MYSQL_DATABASE_DB'] = 'drugdb'
-app.config['MYSQL_DATABASE_HOST'] = 'GraceCdeMacBook-Air.local'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 # upload configs
@@ -30,78 +28,91 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CSRF_ENABLED'] = True
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-
+# file format check ########TO DO#######
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# upload page
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        evidence_name = request.form['evidence']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            saveLocation = app.config['UPLOAD_FOLDER'] + filename
-            file.save(saveLocation)
+        try:
+                file = request.files['file']
+                evidence_name = request.form['evidence']
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    saveLocation = app.config['UPLOAD_FOLDER'] + filename
+                    file.save(saveLocation)
             #file.save(filepath)
             #token = store_in_db(filepath)
             #return redirect(url_for('read_uploaded_file',
             #                        filename=filename))
-            df1 = pd.read_csv(saveLocation)
-            engine = create_engine('mysql://root:binf3111@localhost/drugdb', echo=True)
-            print "I've created engine"
-            df1.to_sql(evidence_name, con=engine, if_exists='replace')
-            return 'file uploaded successfully'
-    return render_template('upload.html')
+                    df1 = pd.read_csv(saveLocation)
+                    engine = create_engine('mysql://root:binf3111@localhost/drugdb', echo=True)
+                    df1.to_sql(evidence_name, con=engine, if_exists='replace')
+                    return render_template('upload_success.html')
+        except Exception as e:
+            return render_template('error.html', error = 'No file selected or no evidence entered!')
+    else:
+        return render_template('upload.html')
 
+# main page
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
 
+# search page
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    #_ = request.form['search']
-    #search = request.args.get('Search')
-    #password = request.args.get('Password')
-    #cursor = mysql.connect().cursor()
-    #cursor.execute("SELECT * from drugdb.1")
-    #return jsonify(data=cursor.fetchall())
-    #data = cursor.fetchall()
-    #return data
-    #for row in data:
-    #    print(row)
-    #if data is None:
-    # return "Username or Password is wrong"
-    #else:
-    # return "Logged in successfully"
     if request.method == 'POST':
-         #try:
-       _query = request.form['query']
-       _evidence = request.form['evidence']
-       conn = mysql.connect()
-       cursor = conn.cursor()
-       cursor.callproc('findEvidenceOne', _query, _evidence)
-       if _query:
-            return json.dumps({'html':'<span>All fields good !!</span>'})
-       else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
-
+       try:
+                _query = request.form['query']
+                _evidence = request.form['evidence']
+                return redirect(url_for('result', query=_query, evidence = _evidence))
+       except Exception as e:
+                return render_template('error.html', error= 'Drug or evidence name is missing.')
+       
+       
     else:
         form = SearchForm()
-        if form.validate_on_submit():
-            flash('Search requested for query="%s"' %
-                  (form.query.data))
-            return redirect(url_for('result', query=form.query.data), evidence=form.evidence.data)
+        #if form.validate_on_submit():
+        #    flash('Search requested for query="%s"' %
+        #          (form.query.data))
+        #    return redirect(url_for('result', query=form.query.data), evidence=form.evidence.data)
         return render_template('search.html',
                                title='Search',
                                form=form)
 
 
-@app.route('/result/<query>', methods=['GET','POST'])
-def result(query, evidence):
-  return render_template('result.html', title='Result', keyword=keyword)
+# result page
+@app.route('/result/<query>/<evidence>', methods=['GET','POST'])
+def result(query,evidence):
+  # parse data 
+    #print data
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    args = (query, evidence)
+    cursor.callproc('searchOneEvidence', args)
+    data = cursor.fetchall()
+    conn.close()
 
+    print data
+    print type(data)
+    datadf = pd.DataFrame(list(data), columns=['Name', 'Score'])
+    datadf.set_index(['Name'],inplace = True)
+    datadf.index.name = None
+    print datadf
+    data_table = datadf.to_html()
+    titles = ['na']
+    titles.append('Drugs with evidence '+ evidence)
+
+    if len(data) >0:
+    # no url change now
+        return render_template('result.html',tables=[datadf.to_html(classes='male')], titles = titles)
+    else:
+        return render_template('error.html', error = 'No result!')
+    
 #@app.route('/showSignUp')
 #def showSignUp():
 #    return render_template('signup.html')
--- combined/db_codes/procedure.sql
+++ combined/db_codes/procedure.sql
@@ -1,28 +1,28 @@
-DROP PROCEDURE IF EXISTS searchOneEvidence//
-DELIMITER //
+DROP PROCEDURE IF EXISTS searchOneEvidence;
+DELIMITER $$
 CREATE PROCEDURE searchOneEvidence(IN drugName VARCHAR(255),IN evidenceName VARCHAR(255))
 BEGIN
-  SET @t1 =CONCAT('SELECT `Unnamed: 0`, ',drugName ,' FROM ',evidenceName);
+  SET @t1 =CONCAT('SELECT `Unnamed: 0`, ',drugName ,' FROM ',evidenceName, ' WHERE `Unnamed: 0` != "',drugName, '"');
   PREPARE result FROM @t1;
   EXECUTE result;
   DEALLOCATE PREPARE result;
-END; 
-//
+END$$
+DELIMITER ;
 
 call searchOneEvidence('ANWABDrug','qq')//
 call searchAllEvidence('ANWABDrug')//
 
 #search all evidence with one drugName as a parameter
 #database name = 'mydrugfinaldatabase'
-drop procedure if exists searchAllEvidence//
-DELIMITER //
+drop procedure if exists searchAllEvidence;
+DELIMITER $$
 create procedure searchAllEvidence(IN drugName VARCHAR(255))
 begin
     DECLARE done int default false;
     DECLARE table_name CHAR(255);
 
     DECLARE cur1 cursor for SELECT t.TABLE_NAME FROM information_schema.tables t
-    	WHERE t.table_schema='mydrugfinaldatabase';
+    	WHERE t.table_schema='drugdb';
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
     open cur1;
     myloop: loop
@@ -30,12 +30,13 @@ begin
         if done then
             leave myloop;
         end if;
-        SET @t1 =CONCAT('SELECT `Unnamed: 0`, ',drugName ,' FROM ',TABLE_NAME);
+        SET @t1 =CONCAT('SELECT `Unnamed: 0`, ',drugName ,' FROM ',TABLE_NAME, ' WHERE `Unnamed: 0` != "',drugName, '"');
 		PREPARE result FROM @t1;
 		EXECUTE result;
 		DEALLOCATE PREPARE result;
     end loop;
     close cur1;
-end; 
-//
+end $$
+DELIMITER ;
+
 
