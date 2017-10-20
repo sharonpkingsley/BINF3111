aa0d4aabbbc04bd5cdb6207df411241e91386c24
VivianHsu840329 <vivianhsu329@gmail.com>
2017-10-20 14:38:54 +1100

    ---------some modification------

--- modified_newDrug.sql
+++ modified_newDrug.sql
@@ -28,43 +28,83 @@ begin
     DECLARE cur3_2 cursor for SELECT t.TABLE_NAME, t.COLUMN_NAME FROM information_schema.columns t
         WHERE t.table_schema='drugdb' and t.TABLE_NAME like 'pathway%';
 
+    if not exists(select ID from evia where ID = drugID) THEN 
+        set @row = concat('insert into evia (ID, `GENERIC NAME`) values ( \'', drugID,  '\', \'', drugName,'\')');
+        select @row;
+        PREPARE r FROM @row;
+        EXECUTE r;
+        DEALLOCATE PREPARE r;
+    end if;
+    #add the new row for all the table drugbank_id
+    open cur1_1;
+    begin
+        DECLARE done int default false;
+        DECLARE table_name1_1 CHAR(255);
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        myloop1: loop
+            fetch cur1_1 into table_name1_1; #target0, target1....
+            if done then
+                leave myloop1;
+            end if;
+            SET @tempTable = table_name1_1; 
+            #select @tempTable;
+            set @tempTableID = drugID; 
+            select @tempTableID;
+            set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
+            select @insertFirstID;
+            PREPARE re FROM @insertFirstID;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end loop;
+    end;
+    close cur1_1;
+
+    open cur2_1;
+    begin
+        DECLARE done int default false;
+        DECLARE table_name2_1 CHAR(255);
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        myloop4: loop
+            fetch cur2_1 into table_name2_1; #target0, target1....
+            if done then
+                leave myloop4;
+            end if;
+            SET @tempTable = table_name2_1; 
+            set @tempTableID = drugID; 
+            set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
+            select @insertFirstID;
+            PREPARE re FROM @insertFirstID;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end loop;
+    end;
+    close cur2_1;
+
+    open cur3_1;
+    begin
+        DECLARE done int default false;
+        DECLARE table_name3_1 CHAR(255);
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+        myloop7: loop
+            fetch cur3_1 into table_name3_1;
+            if done then
+                leave myloop7;
+            end if;
+            SET @tempTable = table_name3_1; 
+            #select @tempTable;
+            set @tempTableID = drugID; 
+            set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
+            select @insertFirstID;
+            PREPARE re FROM @insertFirstID;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end loop;
+    end;
+    close cur3_1;
+
 
     #new drug input --> drugID as the table name
     if evi1 = 'target' then 
-        ####update the last row of the info dataset####
-        #######  evia<---->info
-        if not exists(select ID from info where ID = drugID) THEN 
-            set @row = concat('insert into info (ID, `GENERIC NAME`) values ( \'', drugID,  '\', \'', drugName,'\')');
-            select @row;
-            PREPARE r FROM @row;
-            EXECUTE r;
-            DEALLOCATE PREPARE r;
-        end if;
-        #select * from evia;
-        #########
-        #add the new row for all the table drugbank_id
-        open cur1_1;
-        begin
-            DECLARE done int default false;
-            DECLARE table_name1_1 CHAR(255);
-            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-            myloop1: loop
-                fetch cur1_1 into table_name1_1; #target0, target1....
-                if done then
-                    leave myloop1;
-                end if;
-                SET @tempTable = table_name1_1; 
-                #select @tempTable;
-                set @tempTableID = drugID; 
-                select @tempTableID;
-                set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
-                select @insertFirstID;
-                PREPARE re FROM @insertFirstID;
-                EXECUTE re;
-                DEALLOCATE PREPARE re;
-            end loop;
-        end;
-        close cur1_1;#
         #add the column to the file
         open cur1;
         begin
@@ -90,13 +130,13 @@ begin
             set @updateTable = concat('update ', @tempTable1, ' f inner join ( select DRUGBANK_ID, ', drugID ,' from ', @tempTableID1, ' )a on f.DRUGBANK_ID = a.DRUGBANK_ID set f.',@tempTableID1,'= a.', drugID);
             PREPARE re FROM @updateTable;
             EXECUTE re;
-            DEALLOCATE PREPARE re;#
+            DEALLOCATE PREPARE re;
             set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
             PREPARE re FROM @updateToNull;
             EXECUTE re;
             DEALLOCATE PREPARE re;
         end;
-        close cur1; ###
+        close cur1; 
         #find where the latest column store in
         select @tempTable1;
         #double check the present drugID
@@ -125,48 +165,64 @@ begin
             end loop;
         end;
         close cur1_2;
+
+        open cur2;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name2_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop5: loop
+                fetch cur2 into table_name2_2;
+                if done then
+                    leave myloop5;
+                end if;
+                SET @tempTable1 = table_name2_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            #add the new null column for the new drug with its ID as the column name
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur2; 
+
+        open cur3;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name3_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop8: loop
+                fetch cur3 into table_name3_2; #target0, target1....
+                if done then
+                    leave myloop8;
+                end if;
+                SET @tempTable1 = table_name3_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;#
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur3; 
     end if;
 
     if evi1 = 'chemical_structure' then 
-        ####update the last row of the info dataset####
-        #######  evia<---->info
-        if not exists(select ID from info where ID = drugID) THEN 
-            set @row = concat('insert into info (ID, `GENERIC NAME`) values ( \'', drugID,  '\', \'', drugName,'\')');
-            select @row;
-            PREPARE r FROM @row;
-            EXECUTE r;
-            DEALLOCATE PREPARE r;
-        end if;
-        #select * from evia;
-        #########
-        #add the new row for all the table drugbank_id
-        open cur2_1;
-        begin
-            DECLARE done int default false;
-            DECLARE table_name2_1 CHAR(255);
-            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-            myloop4: loop
-                fetch cur2_1 into table_name2_1; #target0, target1....
-                if done then
-                    leave myloop4;
-                end if;
-                SET @tempTable = table_name2_1; 
-                set @tempTableID = drugID; 
-                ################### for testing to delete all teh added rows ####################
-                #set @deleteRow = concat('delete from ',@tempTable,' where DRUGBANK_ID = \'',@tempTableID,'\'');
-                #PREPARE re FROM @deleteRow;
-                #EXECUTE re;
-                #DEALLOCATE PREPARE re;
-                ##################################################################################
-                ##################################################################################
-                set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
-                select @insertFirstID;
-                PREPARE re FROM @insertFirstID;
-                EXECUTE re;
-                DEALLOCATE PREPARE re;
-            end loop;
-        end;
-        close cur2_1;
         #add the column to the file
         open cur2;
         begin
@@ -224,43 +280,64 @@ begin
             end loop;
         end;
         close cur2_2;
+
+        open cur1;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name1_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop2: loop
+                fetch cur1 into table_name1_2; #target0, target1....
+                if done then
+                    leave myloop2;
+                end if;
+                SET @tempTable1 = table_name1_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            #add the new null column for the new drug with its ID as the column name
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;#
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur1; 
+
+        open cur3;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name3_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop8: loop
+                fetch cur3 into table_name3_2; #target0, target1....
+                if done then
+                    leave myloop8;
+                end if;
+                SET @tempTable1 = table_name3_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;#
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur3; 
     end if;
 
     if evi1 = 'pathway' then 
-        ####update the last row of the info dataset####
-        #######  evia<---->info
-        if not exists(select ID from info where ID = drugID) THEN 
-            set @row = concat('insert into info (ID, `GENERIC NAME`) values ( \'', drugID,  '\', \'', drugName,'\')');
-            select @row;
-            PREPARE r FROM @row;
-            EXECUTE r;
-            DEALLOCATE PREPARE r;
-        end if;
-        #select * from evia;
-        #########
-        #add the new row for all the table drugbank_id
-        open cur3_1;
-        begin
-            DECLARE done int default false;
-            DECLARE table_name3_1 CHAR(255);
-            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-            myloop7: loop
-                fetch cur3_1 into table_name3_1;
-                if done then
-                    leave myloop7;
-                end if;
-                SET @tempTable = table_name3_1; 
-                #select @tempTable;
-                set @tempTableID = drugID; 
-                set @insertFirstID = concat('insert into ', @tempTable, '(DRUGBANK_ID) values (\'', drugID ,'\')');
-                select @insertFirstID;
-                PREPARE re FROM @insertFirstID;
-                EXECUTE re;
-                DEALLOCATE PREPARE re;
-            end loop;
-        end;
-        close cur3_1;
-
         #add the column to the file
         open cur3;
         begin
@@ -318,6 +395,62 @@ begin
             end loop;
         end;
         close cur3_2;
+
+        open cur1;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name1_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop2: loop
+                fetch cur1 into table_name1_2; #target0, target1....
+                if done then
+                    leave myloop2;
+                end if;
+                SET @tempTable1 = table_name1_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            #add the new null column for the new drug with its ID as the column name
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur1; 
+
+        open cur2;
+        begin
+            DECLARE done int default false;
+            DECLARE table_name2_2 CHAR(255);
+            DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+            myloop5: loop
+                fetch cur2 into table_name2_2;
+                if done then
+                    leave myloop5;
+                end if;
+                SET @tempTable1 = table_name2_2;
+            end loop;
+            #add to the end of the last file of the evidence file
+            #select @tempTable1;
+            #add the new null column for the new drug with its ID as the column name
+            set @tempTableID1 = drugID; #new uplaod file for the new drug
+            #select @tempTableID;#
+            set @tt = concat('alter table ', @tempTable1, ' add column ', @tempTableID1,' VARCHAR(255)');
+            PREPARE res FROM @tt;
+            EXECUTE res;
+            DEALLOCATE PREPARE res;
+            set @updateToNull = concat('update ', @tempTable1, ' set ',drugID ,' = 0 where ',drugID,' is null');
+            PREPARE re FROM @updateToNull;
+            EXECUTE re;
+            DEALLOCATE PREPARE re;
+        end;
+        close cur2; 
     end if;
 
     set @dropInputTable = concat('drop table if exists ', drugID);
@@ -331,7 +464,7 @@ DELIMITER ;
 
 
 
-#call newDrugAddition('DB30000','ADCCCC','Target');
+call newDrugAddition('DB30000','ADCCCC','pathway');
 
 
 #delete from evia where ID = 'DB30000';
